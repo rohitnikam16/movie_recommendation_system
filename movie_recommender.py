f3dff602242ca9dac8a8d41449175256718ae779
Rishabh <rishabh224kumar@gmail.com>
2021-03-20 18:18:59 +0530

    Code refactored and datasets sent to a separate directory

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,55 +1,53 @@
-#importing libraries
+# importing libraries
 import pandas as pd
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
-
-#loading datasets
+# loading datasets
 movies = pd.read_csv("dataset/movies.csv")
 ratings = pd.read_csv("dataset/ratings.csv")
 
-
-#final Dataset
-final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
-final_dataset.fillna(0,inplace=True)
-
+# final Dataset
+final_dataset = ratings.pivot(index='movieId', columns='userId', values='rating')
+final_dataset.fillna(0, inplace=True)
 
 '''removing noise i.e considering only those users who have rated more than 50 movies and 
-considering only those moveies which are rated by more than 10 users.'''
+considering only those movies which are rated by more than 10 users.'''
 no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
 no_movies_voted = ratings.groupby('userId')['rating'].agg('count')
-final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index,:]
-final_dataset = final_dataset.loc[:,no_movies_voted[no_movies_voted > 50].index]
-
+final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index, :]
+final_dataset = final_dataset.loc[:, no_movies_voted[no_movies_voted > 50].index]
 
 '''removing sparsity by using csr_matrix function from the scipy library.'''
 csr_data = csr_matrix(final_dataset.values)
-final_dataset.reset_index(inplace=True)   #indexing the final_dataset dataframe
-
+final_dataset.reset_index(inplace=True)  # indexing the final_dataset dataframe
 
 '''using KNN algorithm to compute similarity with cosine distance metric'''
 knn = NearestNeighbors(metric='cosine', algorithm='auto', n_neighbors=20, n_jobs=-1)
 knn.fit(csr_data)
 
-'''
- We first check if the movie_name input is in the database and if it is 
- we use our recommendation system to find similar movies and sort them 
- based on their similarity distance and output only the top 10 movies with 
- their distances from the input movie. '''
+
 def get_movie_recommendation(movie_name):
-    n_movies_to_reccomend = 10
+    """
+    We first check if the movie_name input is in the database and if it is
+    we use our recommendation system to find similar movies and sort them
+    based on their similarity distance and output only the top 10 movies with
+    their distances from the input movie.
+    """
+    n_movies_to_recommend = 10
     movie_list = movies[movies['title'].str.contains(movie_name)]
     if len(movie_list):
-        movie_idx= movie_list.iloc[0]['movieId']
+        movie_idx = movie_list.iloc[0]['movieId']
         movie_idx = final_dataset[final_dataset['movieId'] == movie_idx].index[0]
-        distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)
-        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[1:]
+        distances, indices = knn.kneighbors(csr_data[movie_idx], n_neighbors=n_movies_to_recommend + 1)
+        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(), distances.squeeze().tolist())),
+                                   key=lambda x: x[1])[1:]
         recommend_frame = []
         for val in rec_movie_indices:
             movie_idx = final_dataset.iloc[val[0]]['movieId']
             idx = movies[movies['movieId'] == movie_idx].index
-            recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0],'Distance':val[1]})
-        df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
+            recommend_frame.append({'Title': movies.iloc[idx]['title'].values[0], 'Distance': val[1]})
+        df = pd.DataFrame(recommend_frame, index=range(1, n_movies_to_recommend + 1))
         return df
     else:
         return "No movies found. Please check your input"
